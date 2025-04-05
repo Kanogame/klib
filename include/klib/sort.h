20d977ce1b004056c8028170c9a9e7305ce87fc4
kanogame <wasdqert1222@gmail.com>
2025-04-05 18:21:54 +0700

    improvements to sortings

--- include/klib/sort.h
+++ include/klib/sort.h
@@ -1,6 +1,11 @@
 #ifndef KLIB_SORT
 #define KLIB_SORT
 
+typedef struct sort_Stats {
+  int swaps;
+  int comparisons;
+} sort_Stats;
+
 // bigger  >
 void sort_bubble(int *arr, int size);
 
@@ -11,8 +16,12 @@ void sort_binaryInsertion(int *arr, int size);
 
 void sort_cocktail(int *arr, int size);
 
+sort_Stats *sort_bubbleStats(int *arr, int size);
+
+sort_Stats *sort_cocktailStats(int *arr, int size);
+
 // generic implementation with comparator fuctions
-int sort_bubbleComp(void **arr, int size, int (*bigger)(void *, void *));
+void sort_bubbleComp(void **arr, int size, int (*bigger)(void *, void *));
 void sort_insertionComp(void **arr, int size, int (*bigger)(void *, void *));
 void sort_selectionComp(void **arr, int size, int (*bigger)(void *, void *));
 #endif
--- src/sort/sort.c
+++ src/sort/sort.c
@@ -1,4 +1,6 @@
+#include "../../include/klib/sort.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void sort_bubble(int *arr, int size) {
   int flag, tmp;
@@ -82,32 +84,117 @@ void sort_binaryInsertion(int *arr, int size) {
 }
 
 void sort_cocktail(int *arr, int size) {
-  int flag = 1;
+  int new_start, new_end;
+
   int tmp = 0;
-  while (flag) {
-    flag = 0;
-    for (int i = 0; i < size - 1; i++) {
+
+  int start = 0;
+  int end = size - 1;
+  while (start < end) {
+    new_end = start;
+    for (int i = start; i < end; i++) {
       if (arr[i] > arr[i + 1]) {
         tmp = arr[i];
         arr[i] = arr[i + 1];
         arr[i + 1] = tmp;
-        flag = 1;
+        new_end = i;
       }
     }
-    if (!flag) {
+
+    end = new_end;
+
+    if (start >= end) {
       break;
     }
 
-    flag = 0;
-    for (int i = size - 1; i >= 0; i--) {
+    new_start = end;
+
+    for (int i = end; i >= start; i--) {
       if (arr[i] < arr[i - 1]) {
         tmp = arr[i];
         arr[i] = arr[i - 1];
         arr[i - 1] = tmp;
+        new_start = 1;
+      }
+    }
+
+    start = new_start;
+  }
+}
+
+sort_Stats *sort_bubbleStats(int *arr, int size) {
+  int comp = 0;
+  int swaps = 0;
+
+  int flag, tmp;
+  for (int i = 0; i < size - 1; i++) {
+    flag = 0;
+    for (int j = 0; j < size - 1; j++) {
+      comp++;
+      if (arr[j] > arr[j + 1]) {
+        tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
         flag = 1;
+        swaps++;
       }
     }
   }
+
+  sort_Stats *res = malloc(sizeof(sort_Stats));
+  res->comparisons = comp;
+  res->swaps = swaps;
+  return res;
+}
+
+sort_Stats *sort_cocktailStats(int *arr, int size) {
+  int comp = 0;
+  int swaps = 0;
+  int new_start, new_end;
+
+  int tmp = 0;
+
+  int start = 0;
+  int end = size - 1;
+  while (start < end) {
+    new_end = start;
+    for (int i = start; i < end; i++) {
+      comp++;
+      if (arr[i] > arr[i + 1]) {
+        tmp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = tmp;
+        new_end = i;
+        swaps++;
+      }
+    }
+
+    end = new_end;
+
+    if (start >= end) {
+      break;
+    }
+
+    new_start = end;
+
+    for (int i = end; i >= start; i--) {
+      comp++;
+      if (arr[i] < arr[i - 1]) {
+        tmp = arr[i];
+        arr[i] = arr[i - 1];
+        arr[i - 1] = tmp;
+        new_start = 1;
+        swaps++;
+      }
+    }
+
+    start = new_start;
+  }
+
+  sort_Stats *res = malloc(sizeof(sort_Stats));
+  res->comparisons = comp;
+  res->swaps = swaps;
+  return res;
 }
 
 void sort_bubbleComp(void **arr, int size, int (*bigger)(void *, void *)) {
