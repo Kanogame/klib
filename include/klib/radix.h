fd55a6d7737a9e2de98a751f48de8e57b4a18179
kanogame <wasdqert1222@gmail.com>
2025-05-03 22:47:23 +0700

    radix bin msd & str msd

--- include/klib/radix.h
+++ include/klib/radix.h
@@ -1,7 +1,9 @@
 #ifndef KLIB_MATH_RADIX
 #define KLIB_MATH_RADIX
+#include <klib/sort.h>
 
-void msd_binary_sort(int *array, int size, int digit);
-void msd_sort(char **arr, int start, int end, int digit, int maxDigits);
+void radix_msd_binary(int *array, int size, int digit);
+sort_Stats *radix_msd_binary_stats(int *array, int size, int digit);
+void radix_msd_stings(char **a, int start, int end, int digit, int maxDigit);
 
 #endif
--- src/sort/radix.c
+++ src/sort/radix.c
@@ -1,11 +1,14 @@
+#include <klib/error.h>
 #include <klib/macros.h>
+#include <klib/sort.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-static int bin(int value, int digit) { return ((value >> digit) & 1); }
+inline static int bin(int value, int digit) { return ((value >> digit) & 1); }
 
 // fast binary sort
-void msd_binary_sort(int *array, int size, int digit) {
+void radix_msd_binary(int *array, int size, int digit) {
   if (digit < 0 || size == 0) {
     return;
   }
@@ -24,60 +27,91 @@ void msd_binary_sort(int *array, int size, int digit) {
     }
   }
 
-  msd_binary_sort(array, i, digit - 1);
-  msd_binary_sort(array + i, size - i, digit - 1);
+  radix_msd_binary(array, i, digit - 1);
+  radix_msd_binary(array + i, size - i, digit - 1);
 }
 
-static const int RADIX = 255; // ASCII
+sort_Stats *radix_msd_binary_stats(int *array, int size, int digit) {
+  if (digit < 0 || size == 0) {
+    return malloc(sizeof(sort_Stats));
+  }
 
-void msd_sort(char **arr, int start, int end, int digit, int maxDigits) {
-  if (end - start <= 1 || digit >= maxDigits)
+  int i = 0;
+  int j = size - 1;
+  int cmp = 0;
+  int swaps = 0;
+  while (i <= j) {
+    cmp++;
+    if (!bin(array[i], digit)) {
+      i++;
+    } else if (bin(array[j], digit)) {
+      j--;
+    } else {
+      XOR_SWAP(array[i], array[j]);
+      swaps++;
+      i++;
+      j--;
+    }
+    cmp++;
+  }
+
+  sort_Stats *val1 = radix_msd_binary_stats(array, i, digit - 1);
+  sort_Stats *val2 = radix_msd_binary_stats(array + i, size - i, digit - 1);
+  val1->comparisons += val2->comparisons + cmp;
+  val1->swaps += val2->swaps + swaps;
+  return val1;
+}
+
+#define R 256 // ASCII
+
+#define CUTOFF 15
+
+void radix_msd_stings(char **a, int start, int end, int digit, int maxDigit) {
+  if (end <= start || digit >= maxDigit)
     return;
 
-  int *count = calloc(RADIX + 1, sizeof(int));
-  int i;
-
-  int pos = maxDigits - 1 - digit;
-  for (i = start; i < end; i++) {
-    char val = arr[i][pos];
-    count[val + 1]++;
+  int *count = calloc(R + 1, sizeof(int));
+  if (count == NULL) {
+    EXIT_WITH_ERROR("err");
   }
 
-  // pref sum
-  for (i = 0; i < RADIX; i++) {
-    count[i + 1] += count[i];
+  for (int i = start; i < end; ++i) {
+    int c = a[i][digit];
+    count[c + 1]++;
   }
 
-  // Allocate temporary array for sorted order of current digit pass
-  int size = end - start;
-  char **aux = malloc(size * sizeof(char *));
+  int *bound = NULL;
+  int bsize = 0;
 
-  // Distribute: place elements in appropriate positions
-  for (i = start; i < end; i++) {
-    char val = arr[i][pos];
-    aux[count[val]++] = arr[i];
-  }
-
-  // Copy back to original array
-  for (i = 0; i < size; i++) {
-    arr[start + i] = aux[i];
-  }
-  free(aux);
-
-  int boundaries[RADIX + 1];
-  boundaries[0] = start;
-  for (i = 0; i < RADIX; i++) {
-    int bucketCount = 0;
-    for (int j = start; j < end; j++) {
-      char val = arr[j][pos];
-      if (val == i)
-        bucketCount++;
+  for (int r = 0; r < R; r++) {
+    count[r + 1] += count[r];
+    if (count[r + 1] - count[r] > 1) {
+      bsize += 2;
+      bound = realloc(bound, bsize * sizeof(int));
+      bound[bsize - 2] = start + count[r];
+      bound[bsize - 1] = start + count[r + 1];
     }
-    boundaries[i + 1] = boundaries[i] + bucketCount;
   }
 
-  // Recursively sort each bucket on the next digit.
-  for (i = 0; i < RADIX; i++) {
-    msd_sort(arr, boundaries[i], boundaries[i + 1], digit + 1, maxDigits);
+  int size = end - start;
+  char **aux = calloc(size, sizeof(char *));
+  if (aux == NULL) {
+    EXIT_WITH_ERROR("err");
   }
+
+  for (int i = start; i < end; ++i) {
+    int c = a[i][digit];
+    aux[count[c]++] = a[i];
+  }
+  for (int i = 0; i < size; i++) {
+    a[start + i] = aux[i];
+  }
+
+  free(aux);
+  free(count);
+
+  for (int r = 0; r < bsize / 2; r++) {
+    radix_msd_stings(a, bound[r * 2], bound[r * 2 + 1], digit + 1, maxDigit);
+  }
+  free(bound);
 }
